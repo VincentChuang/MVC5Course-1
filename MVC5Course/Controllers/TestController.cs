c4f7811d08e41bd0253030cc3f8a5b071fd11fcb
Vincent Chuang <laba.chuang@gmail.com>
2017-12-02 17:35:25 +0800

    08 建立新的 TestController 並練習透過 Entity Framework 操作 CRUD 動作

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+
+        #region Index - 列表
+        public ActionResult Index()
+        {
+            var data = from p in db.Product.Take(10)
+                       select p;                //所得所有資料
+
+            return View(data);
+        }
+        #endregion
+
+        #region Create - 建立
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //打 mvcp 即可出現
+        [HttpPost]
+        public ActionResult Action(Product data)    
+        {
+            if (ModelState.IsValid) {
+                db.Product.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        #endregion
+
+        #region Edit - 編輯
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id , Product data)
+        {
+            //ValueInjecter 套件 ==> 
+            if (ModelState.IsValid) {
+                var updData = db.Product.Find(id);
+                if (updData != null) {
+                    //updData = data;
+                    //updData.ProductId = id;
+                    updData.ProductName = data.ProductName;
+                    updData.Active = data.Active;
+                    updData.Price = data.Price;
+                    updData.Stock = data.Stock;
+                }
+                int iFect = db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+        #endregion
+
+        #region Delete - 刪除
+        public ActionResult Delete(int id) {
+            var listOrderLine = db.OrderLine.Where(x => x.ProductId == id).ToList();
+            if(listOrderLine!=null && listOrderLine.Count > 0) {
+                db.OrderLine.RemoveRange(listOrderLine);
+            }
+            var delP = db.Product.Where(x => x.ProductId == id).FirstOrDefault();
+            if (delP != null) {
+                db.Product.Remove(delP);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Details - 顯示明細
+        public ActionResult Details(int id) {
+            var detailP = db.Product.Where(x => x.ProductId == id).FirstOrDefault();
+            return View(detailP);
+        }
+        #endregion
+
+
+    }
+}
