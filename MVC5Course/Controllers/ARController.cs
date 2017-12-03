b105e10e7cade270356bb99029ef37ae992c53d1
Vincent Chuang <laba.chuang@gmail.com>
2017-12-03 15:55:59 +0800

    整理相關 註解

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -18,20 +18,28 @@ namespace MVC5Course.Controllers
             return PartialView("Index");
         }
 
+
+        #region 實做 彈出Alert提示後，指定轉向首頁，兩種寫法
+
+        //不好的寫法，因在 Controller 中 撰寫 html code
         public ActionResult ContentTest() {
             return Content("<script>alert('OK');location.href='/';</script>");
         }
+        //較好的寫法，指定 PartialView 且 在 Shared 中 撰寫共用 JsAlertRedirect.cshtml
         public ActionResult ContentTest_Better() {
             return PartialView("JsAlertRedirect", "新增成功");
         }
 
+        #endregion
 
 
         public ActionResult FileTest(string dl) {
             if (String.IsNullOrEmpty(dl) == true) {
+                //File Result，第三參數未指定，代表 透過 browser 呈現
                 return File(Server.MapPath("~/App_Data/A01.png"), "image/png");
             } else {
                 //當為下載時候，需指定檔名
+                //File Result，第三參數有指定，代表下載該指定檔名檔案
                 return File(Server.MapPath("~/App_Data/A01.png"), "image/png", "GreenIsland_綠島.png");
             }
         }
