ca3fd9d074ae8099f1e66971d8b41c4a7b9ccbf1
Vincent Chuang <laba.chuang@gmail.com>
2017-12-03 15:50:13 +0800

    16 練習 FileResult 的使用方式 ( 記得查詢正確的 Content-Type 設定值 ) 15 實作 JsAlertRedirect 共用檢視 (Shared View)

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -9,16 +9,35 @@ namespace MVC5Course.Controllers
     public class ARController : Controller
     {
 
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
         //測試 Partial View
-        public ActionResult PartialViewTest()
-        {
+        public ActionResult PartialViewTest() {
             return PartialView("Index");
         }
+
+        public ActionResult ContentTest() {
+            return Content("<script>alert('OK');location.href='/';</script>");
+        }
+        public ActionResult ContentTest_Better() {
+            return PartialView("JsAlertRedirect", "新增成功");
+        }
+
+
+
+        public ActionResult FileTest(string dl) {
+            if (String.IsNullOrEmpty(dl) == true) {
+                return File(Server.MapPath("~/App_Data/A01.png"), "image/png");
+            } else {
+                //當為下載時候，需指定檔名
+                return File(Server.MapPath("~/App_Data/A01.png"), "image/png", "GreenIsland_綠島.png");
+            }
+        }
+
+
+
 
 
     }
